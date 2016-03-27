6eef324f631c48aff998c0bcb508c065682f4abe
Latheesh <latheesh.padukana@rea-group.com>
2016-03-27 16:05:25 +1100

    Allow svg, png and plain formats

--- main.go
+++ main.go
@@ -9,24 +9,32 @@ import (
 	"strings"
 )
 
+func renderDot(rw http.ResponseWriter, r *http.Request, format string) {
+	query, err := url.QueryUnescape(r.URL.RawQuery)
+	if err != nil {
+		http.Error(rw, "Bad Request", 400)
+	}
+
+	dotCommand := exec.Command("dot", "-T"+format)
+	dotCommand.Stdin = strings.NewReader(query)
+	dotCommand.Stdout = rw
+	dotCommand.Stderr = os.Stderr
+
+	err = dotCommand.Run()
+	if err != nil {
+		http.Error(rw, err.Error(), 500)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/dot/to/svg", func(rw http.ResponseWriter, r *http.Request) {
-		query, err := url.QueryUnescape(r.URL.RawQuery)
-		if err != nil {
-			http.Error(rw, "Bad Request", 400)
-		}
-
-		dotCommand := exec.Command("dot", "-Tsvg")
-		dotCommand.Stdin = strings.NewReader(query)
-		dotCommand.Stdout = rw
-		dotCommand.Stderr = os.Stderr
-
-		err = dotCommand.Run()
-		if err != nil {
-			http.Error(rw, err.Error(), 500)
-			return
-		}
-	})
-
+	toFormats := []string{"svg", "png", "plain"}
+	for _, toFormat := range toFormats {
+		// need a local copy or the format will point to the last format
+		var format = toFormat
+		http.HandleFunc("/dot/to/"+format, func(rw http.ResponseWriter, r *http.Request) {
+			renderDot(rw, r, format)
+		})
+	}
 	log.Println(http.ListenAndServe(":9292", nil))
 }
